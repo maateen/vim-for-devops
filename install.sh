093ef7a552d0c8b694f9f9908e793159ec2d5725
Maksudur Rahman Maateen <maateen@outlook.com>
2020-03-20 21:58:12 +0600

    updating install script
    Signed-off-by: Maksudur Rahman Maateen <maateen@outlook.com>

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
 #! /bin/bash
 
-set -x
-
 declare -r PWD=$(pwd)
 declare -r WHOAMI=$(whoami)
-declare -r SSH_DIR=~/.ssh
+declare -r PLUG_DIR=~/.vim/autoload/plug.vim
 
 #################
 ### Setup vimrc ###
 #################
-if $(which vim); then
+if $(hash vim); then
+    if [[ ! -d $PLUG_DIR ]]; then
+        curl  --silent -fLo $PLUG_DIR --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    fi
     ln -fFs $(pwd)/vimrc ~/.vimrc
     vim +PlugInstall +qall
 else
