95264b3a825050c113088c467b9fbc6d623bb4bc
Maksudur Rahman Maateen <maateen@outlook.com>
2020-03-21 10:33:26 +0600

    fixing installation bug
    Signed-off-by: Maksudur Rahman Maateen <maateen@outlook.com>

--- install.sh
+++ install.sh
@@ -2,14 +2,14 @@
 
 declare -r PWD=$(pwd)
 declare -r WHOAMI=$(whoami)
-declare -r PLUG_DIR=~/.vim/autoload/plug.vim
+declare -r VIM_PLUG=~/.vim/autoload/plug.vim
 
 #################
 ### Setup vimrc ###
 #################
 if $(hash vim); then
-    if [[ ! -d $PLUG_DIR ]]; then
-        curl  --silent -fLo $PLUG_DIR --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    if [[ ! -f $VIM_PLUG ]]; then
+        curl  --silent -fLo $VIM_PLUG --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
     fi
     ln -fFs $(pwd)/vimrc ~/.vimrc
     vim +PlugUpgrade +PlugClean +PlugInstall +PlugUpdate +qall
